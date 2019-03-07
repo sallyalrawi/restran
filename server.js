d8841f39fd8ffab0d2233c2a772bad42f49ccc17
Ivan Mendoza <theeachtra2002@yahoo.com>
2019-03-06 20:20:10 -0600

    with var

--- server.js
+++ server.js
@@ -12,7 +12,30 @@ var PORT = process.env.PORT || 3005;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+var reservations = [
+    {
+      id: "table1",
+      
+      name: "Yoda",
+      eMail: "JediMaster@theforceiswithyou.com",
+      Phone: 214-434-5379,
+      
+    },
+    {
+        id: "table2",
+        name: "Adrian",
+        eMail: "TheAdrian@thisismyemail.com ",
+        Phone: 867-5309,
+        
+      },
+      {
+        id: "table3",
+        name: "Sally",
+        eMail: "Sally@theforceiswithyou.com",
+        Phone: 214-434-8546,
+        
+      },
+  ];
 //Basic routes
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
